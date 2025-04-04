beed2d2d1e9d009e1c35d56ae603b7e28af92cde
andrew chen <andrew.chen@enerlites.com>
2025-04-04 09:32:54 -0700

    update local file 2 azure

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
+'''
+Packages requiremetns:
+a) `pip install msal httpx python-dotenv`
+'''
 import os 
 import pandas as pd
 from sqlalchemy import create_engine
 from IPython.display import display
 import os 
 from dotenv import load_dotenv
-import urllib.parse
+import urllib.parse;
+import requests
+from io import BytesIO
 
 # load env vars from .env
 load_dotenv()
 DB_CONN = f"mssql+pyodbc://sqladmin:{urllib.parse.quote_plus(os.getenv("DB_PASS"))}@{os.getenv("DB_SERVER")}/master?driver=ODBC+Driver+17+for+SQL+Server&encrypt=yes"
+
+def get_oneDrive_File(url, access_token):
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+        'Accept': 'application/json'
+    }
+    res = requests.get(url, headers=headers)
+    return BytesIO(res.content)         # return an xlsx file 
 
 def oneDrive_2_azure(fp, sheet, table, table_cols):
     df = pd.read_excel(fp,sheet, usecols= lambda x: not x.startswith("Unnamed"))
@@ -34,6 +48,9 @@ def oneDrive_2_azure(fp, sheet, table, table_cols):
         print(f"Error writing to database: {str(e)}")
     finally:
         engine.dispose()
+
+def googleDrive_2_azure(fp, sheet, table, table_cols):
+    pass
 
 if __name__ == '__main__':
     sku_baseCols = ['sku','category','promo_reason','descrip','moq','socal', 'ofs','free_sku','feb_sales','inv_quantity','inv_level','sys_dt']
